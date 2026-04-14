cef58f1b3a9684b9250c6eed4c7a2e9b4080cbac
KingMafia-cell <nyaberilevi@gmail.com>
2026-04-14 12:38:34 +0300

    Update appId in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.5646c66f20214ace8e87e7c6075a3b65',
+  appId: 'com.kingmafia.codebloomnext',
   appName: 'codebloom-next',
-  webDir: 'dist',
-  server: {
-    url: 'https://5646c66f-2021-4ace-8e87-e7c6075a3b65.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
